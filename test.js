6443fd507f6110eebbdf845cdaadb7011bca1fbf
狼叔 <langshu.ssl@alibaba-inc.com>
2018-12-18 10:25:57 +0800

    refact, please ignore

--- test.js
+++ test.js
@@ -1,43 +1,32 @@
-var fs = require('fs');
-var http = require('http');
-// var http = require('./response')
+const fs = require('fs')
+const http = require('http')
+const through = require('.')
 
-var through = require('.')
-
-var server = http.createServer(function (req, res) {
+const server = http.createServer(function (req, res) {
     through(res)
 
-    // var res = Object.create(response)
     if (req.url === '/') {
-        res.writeHead(200);
-        res.end('hello world\n');
+        res.writeHead(200)
+        res.end('hello world\n')
     }
 
     if (req.url === '/json') {
         res.json({
             a: 1
-        });
+        })
     }
 
     if (req.url === '/s') {
-        // res.stream(fs.createReadStream('./index.js'));
-        // res.stream(fs.createReadStream('./package.json'));
-        var that = res
-
-        res.stream.add(fs.createReadStream('./package.json'));
-
+        res.stream.add(fs.createReadStream('./package.json'))
         res.stream.add(fs.createReadStream('./index.js'))
-        
-        res.end();
+        res.end()
     }
 
     if (req.url === '/d') {
-        res.write(fs.createReadStream('./package.json'));
-
+        res.write(fs.createReadStream('./package.json'))
         res.write(fs.createReadStream('./index.js'))
-        
-        res.end();
+        res.end()
     }
 })
 
-server.listen(8000);
+server.listen(8000)
