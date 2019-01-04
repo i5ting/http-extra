9e921e2b2b58a4dd9c410ad0004544dc1e616f35
狼叔 <langshu.ssl@alibaba-inc.com>
2019-01-04 14:34:24 +0800

    add koa for prepare test

---
+++ test-koa.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+const http = require('http')
+const Koa = require('koa');
+const app = new Koa();
+const through = require('.')
+// logger
+
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get('X-Response-Time');
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+// x-response-time
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// response
+
+app.use(async ctx => {
+    const res = ctx.res
+    through(res)
+    res.write('hello\n')
+    res.write(fs.createReadStream('./package.json'))
+    res.write(fs.createReadStream('./index.js'))
+    res.write('world\n')
+    res.end()
+  ctx.body = null;
+});
+
+app.listen(3000);
